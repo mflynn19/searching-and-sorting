7d8d5c67868831374b01c3dc537298d83d4019db
Meghan <meghan.flynn@theflynn.org>
2019-03-04 20:03:58 -0500

    dying

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,11 +1,16 @@
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class SearchAndSort {
-	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		SearchAndSort search = new SearchAndSort();
+
 		System.out.println("What algorithm would you like to execute? bubble, selection, insertion, merge, linear, binary? Type \"quit?\" to exit");
 		String type = in.nextLine().toLowerCase();
 		String data;
@@ -30,67 +35,59 @@ public class SearchAndSort {
 		do {
 			System.out.println("What are the values you would like in the list? Only numbers and commas are allowed; no spaces.");
 			numbers = in.nextLine();
-			
 		} 
 		while (!(numbers.matches("[0-9, /,]+")));
+		
+		String[] dataArray = numbers.split(",");
+		String[] strings = numbers.split(",");
 		
 		//actual options of what to do
 		switch(type) {
 			case "bubble":
 				if (data == "integers") {
 					if (store == "array") {
-						String[] convertedNumberArray = numbers.split(",");
-						int[] intArray = new int[convertedNumberArray.length];
-					    for (int i = 0; i < convertedNumberArray.length; i++) {
-					         String numberAsString = convertedNumberArray[i];
-					         intArray[i] = Integer.parseInt(numberAsString);
-					      }
-						bubbleSort(intArray);
+						int ints[] = new int[dataArray.length]; 
+						for (int i = 0; i < dataArray.length; i++) {
+				            ints[i] = Integer.parseInt(dataArray[i]);
 						}
-					else if (store == "list") {
-						System.out.println("What are the values you would like in the list? Only numbers and commas are allowed; no spaces.");
-						String[] convertedNumbersArray = numbers.split(",");
-						List<Integer> convertedNumberList = new ArrayList<Integer>();
-						for (String number : convertedNumbersArray) {
-						    convertedNumberList.add(Integer.parseInt(number.trim()));
-						}
-						bubbleSort(convertedNumberList);
+						System.out.println(Arrays.toString(ints));
+						System.out.println("Bubble Sort: " + Arrays.toString(search.bubbleSort(ints)));
+						break; 
 					}
-			
-				}
+					else if (store == "list") {
+						ArrayList<Integer> newList = new ArrayList<Integer>(strings.length);
+						for (int i = 0; i < strings.length; i++) {
+							newList.add(Integer.parseInt(strings[i])); 
+						}
+						System.out.println("Bubble Sort: " + search.bubble(newList));
+						break; 
+					}
 				else if (data == "strings") {
 					if (store == "array") {
-						///
+						System.out.println("Bubble Sort: " + Arrays.toString(search.bubbleSort(strings)));
+						break; 
 					}
 					else if (store == "list") {
-						///
+						ArrayList<String> nList = new ArrayList<String>(strings.length); 
+						for (int i = 0; i < strings.length; i++) {
+							nList.add(strings[i]);
+						}
 					}
 				}
 				else {
 					System.out.println("Please try again");
 				}
+				}
+				break;
 				
 		case "selection":
 			if (data == "integers") {
 				if (store == "array") {
-					String[] convertedNumberArray = numbers.split(",");
-					int[] intArray = new int[convertedNumberArray.length];
-				    for (int i = 0; i < convertedNumberArray.length; i++) {
-				         String numberAsString = convertedNumberArray[i];
-				         intArray[i] = Integer.parseInt(numberAsString);
-				      }
-					selectionSort(intArray);
+					/// 
 				}
 				else if (store == "list") {
-					System.out.println("What are the values you would like in the list? Only numbers and commas are allowed; no spaces.");
-					String[] convertedNumbersArray = numbers.split(",");
-					List<Integer> convertedNumberList = new ArrayList<Integer>();
-					for (String number : convertedNumbersArray) {
-					    convertedNumberList.add(Integer.parseInt(number.trim()));
-					}
-					selectionSort(convertedNumberList);
+					///
 				}
-			}
 			else if (data == "strings") {
 				if (store == "array") {
 					///
@@ -102,24 +99,31 @@ public class SearchAndSort {
 			else {
 				System.out.println("Please try again");
 			}
+			}
+			break;
+			
 		case "insertion":
 			
 		case "merge":
 			
+			
 		case "linear":
+			
 			
 		case "binary":
 			
 		case "quit":
+			in.close();
 			System.out.println("Goodbye!");
-	}
+			return;
+		}
 	}
 
 	//BUBBLE SORT INTEGER ARRAY 
-	public static int[] bubbleSort(int[] input) {
+	private int[] bubbleSort(int[] input) {
 		for(int i = 0; i < input.length; i++) {
             for(int j = 0; j < input.length; j++) {
-                if(input[i] < input[j]) {
+                if(input[j] > input[j + 1]) {
                     int temp = input[i];
                     input[i] = input[j];
                     input[j] = temp;
@@ -127,47 +131,42 @@ public class SearchAndSort {
             }
         }
 		return input;
-    }
-	//BUBBLE SORT INTEGER LIST
-	public static List<Integer> bubbleSort(List<Integer> input) {
-		for(int i = 0; i < input.size(); i++) {
-            for(int j = 0; j < input.size(); j++) {
-                if(input.get(i) < input.get(j)) {
-                    int temp = input.get(i);
-                    input.set(i, input.get(j));
-                    input.set(j, temp);
-                }
-            }
-        }
-		return input;
-    }
+	}
+	
 	//BUBBLE SORT STRING ARRAY
-	/*public static String bubbleSort(String input) {
-		for(int i = 0; i < input.length(); i++) {
-            for(int j = 0; j < input.length(); j++) {
-                if(input.get(i) < input.valueOf(j)) {
-                    int temp = input.get(i);
-                    input.set(i, input.get(j));
-                    input.set(j, temp);
+	public String[] bubbleSort(String[] input) {
+		int n = input.length; 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (Integer.parseInt(input[j]) > Integer.parseInt(input[j + 1])) { 
+                    String temp = input[j]; 
+                    input[j] = input[j + 1]; 
+                    input[j + 1] = temp; 
+                } 
+        return input; 
+	}
+	
+	//BUBBLE SORT INTEGER LIST
+	public ArrayList<Integer> bubble(ArrayList<Integer> input) {
+		int n = input.size(); 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (input.get(j) > input.get(j + 1)) { 
+                	input.set(j, input.set(j + 1, input.get(j)));
                 }
-            }
-        }
-		return input;
-    }
+        return input; 
+	}
+	
 	//BUBBLE SORT STRING LIST
-	public static List<Integer> bubbleSort(String input) {
-		for(int i = 0; i < input.length(); i++) {
-            for(int j = 0; j < input.length(); j++) {
-            	 if((input.get(i)).compareToIgnoreCase(input.get(j))<0){
-            		 String t = input.get(j);
-            		 input.get(j)=input.get(i);
-            		 arr.get(i)=t;
-            	 }
-
-            }
-        }
-		return input;
-    }*/
+	public ArrayList<String> bubbleSort(boolean test, ArrayList<String> data) {
+		int n = data.size(); 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (Integer.parseInt(data.get(j)) > Integer.parseInt(data.get(j + 1))) { 
+                	data.set(j, data.set(j + 1, data.get(j)));
+                }
+        return data; 
+	}
 	
 	//SELECTION SORT INTEGER ARRAY
 	public static int[] selectionSort(int[] input) {
@@ -199,5 +198,90 @@ public class SearchAndSort {
 		return input;
     }
 	
-	//NEED 2 SELECTION SORTS FOR STRINGS
+	//SELECTION SORT STRING ARRAY
+	public static String[] selectionSort(String [] input) {
+		int n = input.length; 
+        for (int i = 0; i < n-1; i++) { 
+            int min_idx = i; 
+            for (int j = i + 1; j < n; j++) 
+                if (Integer.parseInt(input[j]) < Integer.parseInt(input[min_idx])) 
+                    min_idx = j; 
+  
+            String temp = input[min_idx]; 
+            input[min_idx] = input[i]; 
+            input[i] = temp; 
+        } 
+        return input; 
+	}
+		
+	//SELECTION SORT STRING LIST
+	public ArrayList<String> selectionSortString(ArrayList<String> stringArray){
+		for(int i = 0; i < stringArray.size(); i++) {
+			int minindex = i;
+			
+			for(int j = i +1; j<stringArray.size(); j++) {
+				if(stringArray.get(j).compareTo(stringArray.get(minindex))<0) {
+					minindex = j;
+				}
+			}
+			
+			String temp = stringArray.get(i);
+			stringArray.set(i, stringArray.get(minindex));
+			stringArray.set(minindex, temp);
+		}
+		return stringArray;
+	}
+	
+	//INSERTION SORT INTEGER ARRAY
+	public int[] insertionSort(int[] input) {
+		int n = input.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = input[i]; 
+            int j = i - 1; 
+  
+            while (j >= 0 && input[j] > key) { 
+                input[j + 1] = input[j]; 
+                j = j-1; 
+            } 
+            input[j + 1] = key; 
+        } 
+        return input; 
+	}
+	
+	//INSERTION SORT STRING ARRAY
+	public String[] insertionSort(String[] input) {
+		int n = input.length; 
+        for (int i = 1; i < n; ++i) 
+        { 
+            String key = input[i]; 
+            int j = i - 1; 
+  
+            while (j >= 0 && Integer.parseInt(input[j]) > Integer.parseInt(key)) 
+            { 
+                input[j + 1] = input[j]; 
+                j = j - 1; 
+            } 
+            input[j + 1] = key; 
+        }
+        return input; 
+	}
+		
+	//INSERTION SORT INTEGER LIST
+	public ArrayList<String> insertionSort(ArrayList<String> input){
+		for(int i = 1; i < input.size(); i++) {
+			String key = input.get(i);
+			int j = i-1;
+			
+			while(j >= 0 && input.get(j).compareTo(key) > 0) {
+				input.set(j + 1, input.get(j));
+				j--;
+			}
+			input.set(j + 1, key);
+		}
+		return input;
+	}
+	
+	//INSERTION SORT STRING LIST
+		
 }
+	
